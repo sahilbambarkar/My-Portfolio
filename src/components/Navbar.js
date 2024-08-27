a3b0bccb79301896a1ae5acdb4e8e4882068c22a
sahilbambarkar <162725936+sahilbambarkar@users.noreply.github.com>
2024-08-27 15:31:44 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,7 +22,6 @@ function Navbar() {
         }
     };
 
-    // Handle window resize for mobile view
     useEffect(() => {
         window.addEventListener('resize', handleWindowResize);
         return () => {
@@ -33,7 +32,7 @@ function Navbar() {
     const downloadResume = () => {
         const link = document.createElement('a');
         link.href = '/Sahil_Resume.pdf'; 
-        link.download = 'Resume_Sahil_Bamarkar.pdf'; // Set the name for the downloaded file
+        link.download = 'Resume_Sahil_Bamarkar.pdf'; 
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
@@ -42,11 +41,11 @@ function Navbar() {
     return (
         <>
             <div className="flex justify-between items-center py-[2rem] px-[3.5rem] sm:px-[1.5rem] relative">
-                <img src="/logo..png" alt="Logo" height={100} width={100}  />
+                <img src="/logo..png" alt="Logo" height={100} width={100} />
                 <div className="flex-grow flex justify-center">
                     <ul className="hidden tm:block sm:block md:flex font-[400] text-black dark:text-white dark:font-[300] text-custom-20 gap-[5rem] md:gap-[3rem] lg:gap-4rem">
                         <a
-                             className="tracking-wider hover:after:bg-black dark:hover:after:bg-[#ec6e59;] font-[400]"
+                            className="tracking-wider hover:after:bg-black dark:hover:after:bg-[#ec6e59;] font-[400]"
                             onClick={() => scrollToComponent('skills')}
                         >
                             Skills
@@ -71,19 +70,19 @@ function Navbar() {
                         </a>
                     </ul>
                 </div>
-                <div className="flex items-center">
+                <div className="hidden tm:block sm:block">
                     <button
                         className="ml-4 bg-[#ec6e59] text-white rounded px-4 py-2 hover:bg-[#d95e51]"
                         onClick={downloadResume}
                     >
                         Download Resume
                     </button>
-                    <div className="tm:hidden sm:hidden absolute top-[2rem] right-[3.5rem]">
-                        <GiHamburgerMenu
-                            className="h-6 w-6 cursor-pointer"
-                            onClick={() => setShowMenu(!showMenu)}
-                        />
-                    </div>
+                </div>
+                <div className="tm:hidden sm:hidden absolute top-[2rem] right-[3.5rem]">
+                    <GiHamburgerMenu
+                        className="h-6 w-6 cursor-pointer"
+                        onClick={() => setShowMenu(!showMenu)}
+                    />
                 </div>
                 {showMenu && (
                     <ul className="flex flex-col font-[400] text-black dark:font-[300] text-custom-20 gap-[2rem] md:gap-[3rem] lg:gap-2rem absolute top-[4.5rem] right-[3.5rem] z-10 bg-[#fdd6cf] p-4 shadow-md rounded">
