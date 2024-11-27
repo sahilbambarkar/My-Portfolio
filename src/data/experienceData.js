cec3dea5973e0fb16518905446cb18e1b9e6edf5
sahilbambarkar <162725936+sahilbambarkar@users.noreply.github.com>
2024-11-27 23:28:29 +0530

    Update experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -2,20 +2,28 @@ const expData = [
    
     {
         id: "1",
+        org: "Prodigy Infotech",
+        title: "Full Stack Developer Intern",
+        date: "April 2024 - September 2024",
+        description:
+            "As a Full Stack Developer Intern at Prodigy Infotech, I gained hands-on experience with the MERN stack and Next.js. I contributed to developing responsive web applications, utilizing Next.js for server-side rendering and static site generation to enhance performance and SEO. My role involved collaborating with cross-functional teams to implement features, conduct testing, and ensure high-quality deliverables while following Agile methodologies."
+    }, 
+    {
+        id: "2",
+        org: "Oasis Infobyte",
+        title: "Web Designing and Development Intern",
+        date: "Sept 2024 - Oct 2024",
+        description:
+            "As a Web Designing and Development Intern at Oasis Infobyte, I have gained valuable experience in creating visually appealing websites. I collaborate with clients to understand their needs and deliver high-quality design solutions. My role involves managing project timelines effectively while ensuring client satisfaction through timely delivery of engaging web designs."
+    } ,
+    {
+        id: "3",
         org: "Freelance",
         title: "ReactJS/NextJS Developer",
         date: "2023 -present",
-        description:  
+        description:
             "I have successfully worked in MERN stack and Nextjs gaining expertise in both frontend and backend development. I have also actively contributed to the testing of Node applications, ensuring high-quality deliverables. Utilizing Agile methodology, I effectively collaborate with teams to enhance productivity and ensure seamless project execution.",
-      },  
-      {
-        id: "2",
-        org: "Freelance",
-        title: "Graphic Designer",
-        date: "2023 -present",
-        description:  
-            "As a freelance graphic designer, I have worked independently and collaborated with clients to create visually appealing and impactful designs ideas, and deliver high-quality visual solutions that meet their needs. I have managed my schedule while ensuring timely project delivery and maintaining client satisfaction. ",
-      },  
+    } 
     
 ];
 
