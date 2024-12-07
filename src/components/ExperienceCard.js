9723b3753f02d9552cb821ce951ef488f3363fe3
sahilbambarkar <162725936+sahilbambarkar@users.noreply.github.com>
2024-12-07 22:35:28 +0530

    Update ExperienceCard.js

--- src/components/ExperienceCard.js
+++ src/components/ExperienceCard.js
@@ -2,31 +2,35 @@ import React from 'react';
 
 function Experience({ org, title, date, description }) {
   return (
-     
-      <div className='flex flex-col mt-10 items-center justify-center'>
-        <div className='flex flex-col sm:flex-row gap-10 ml-4 w-full'>
-          <div className='text-lg mb-2 ml-[-3px] mt-[0.1rem]'>
-            <h1 className='flex-1 min-w-[120px] sm:min-w-[92px]'>{org}</h1>
-            <h3 className='text-sm text-center my-1 dark:text-[#ccc]'>{date}</h3>
+    <div className='flex flex-col mt-10 items-center justify-center px-4 sm:px-0'>
+      <div className='bg-[#2b2e33] shadow-lg rounded-lg p-6 w-full sm:w-[90%] lg:w-[80%]'>
+        <div className='flex flex-col sm:flex-row gap-6 relative'>
+          {/* Organization Name and Date */}
+          <div className='flex flex-col items-start'>
+            <h1 className='text-xl font-semibold text-white mb-1'>{org}</h1>
+            <h3 className='text-sm text-gray-400'>{date}</h3>
           </div>
-          <div className='flex relative mt-[5px] flex-col sm:ml-[-29px]  '>
-            <p className="p-[1px] border-black dark:border-white rounded-50 bg-transparent ">
-              <div className='m-1 bg-[#ec6e59] p-[7px] rounded-full'></div>
-            </p>
-            <div className='absolute top-[29px] bottom-[-26px] left-[12px] sm:top-[26px] border-dotted border-l-2 border-black dark:border-white '></div>
+
+          {/* Indicator Line */}
+          <div className='flex items-center justify-center relative'>
+            <div className='flex flex-col items-center'>
+              <div className='m-1 bg-[#ec6e59] p-3 rounded-full'></div>
+              {/* Connection Line */}
+              <div className='border-l-2 border-dotted border-gray-400 h-full' style={{ height: '50px' }}></div>
+            </div>
           </div>
+
+          {/* Title and Description */}
           <div className='flex flex-col mt-1'>
-            <h1 className='text-lg mb-2 font-semibold'>{title}</h1>
-            <p className='sm:w-[80%] font-light text-custom-18 leading-loose text-sm sm:text-justify dark:text-[#ccc]'>
+            <h1 className='text-lg font-semibold text-white'>{title}</h1>
+            <p className='font-light text-gray-300 leading-relaxed text-sm sm:text-base'>
               {description}
             </p>
           </div>
         </div>
       </div>
-   
+    </div>
   );
 }
 
 export default Experience;
-
- 
