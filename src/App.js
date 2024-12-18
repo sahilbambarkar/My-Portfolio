dafad516786db625704866f4c4a213f428690ed2
sahilbambarkar <162725936+sahilbambarkar@users.noreply.github.com>
2024-12-19 01:37:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,21 +6,19 @@ import Projects from './components/Projects';
 import BackgroundView from './Layout/BackgroundView';
 import Skills from './components/Skills';
 
-
 function App() {
-
   return (
     <BackgroundView>
-      <div className='font-poppins select-none text-black bg-white bg-gray-900  dark:text-white  transition duration-500'>
+      <div className='font-poppins select-none text-white bg-gray-900 transition duration-500'>
         <Navbar />
         <Main />
-        <Skills/>
+        <Skills />
         <Projects />
         <Experiences />
         <Contact />
       </div>
     </BackgroundView>
-  )
+  );
 }
 
-export default App
+export default App;
