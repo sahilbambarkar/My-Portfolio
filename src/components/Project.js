ab607e456a1d0556f5ea184f2a0bdf37ffafdaf0
sahilbambarkar <162725936+sahilbambarkar@users.noreply.github.com>
2024-12-19 01:41:51 +0530

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -25,7 +25,7 @@ function Project({ image, name, live, source, desc }) {
                     href={source}
                     target='_blank'
                     rel='noreferrer'
-                    className='flex items-center gap-1 px-4 py-2 border border-black rounded-md text-sm bg-white dark:bg-gray-600 transition-scale hover:scale-105'
+                    className='flex items-center gap-1 px-4 py-2 border border-black rounded-md text-sm text-white bg-gray-600 transition-scale hover:scale-105'
                 >
                     <AiFillGithub className='w-6 h-6' /> Code
                 </a>
