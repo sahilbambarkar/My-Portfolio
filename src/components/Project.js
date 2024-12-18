ae6bb316e8bcbffdaf04edef37a7cd16b42a4832
sahilbambarkar <162725936+sahilbambarkar@users.noreply.github.com>
2024-12-19 00:17:15 +0530

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,7 +4,7 @@ import { GiPlayButton } from 'react-icons/gi';
 
 function Project({ image, name, live, source, desc }) {
     return (
-        <div className='flex flex-col gap-4 bg-[#2b2e33] p-6 rounded-lg shadow-lg'>
+        <div className='flex flex-col gap-4 bg-gray-800 p-6 rounded-lg shadow-lg'>
             <img
                 alt='project'
                 src={image}
@@ -17,7 +17,7 @@ function Project({ image, name, live, source, desc }) {
                     href={live}
                     target='_blank'
                     rel='noreferrer'
-                    className='flex items-center bg-[#ec6e59] text-white px-4 py-2 rounded-md text-sm'
+                    className='flex items-center bg-[#ec6e59] text-white px-4 py-2 rounded-md text-sm transition-scale hover:scale-105'
                 >
                     <GiPlayButton className='w-6 h-6' /> Demo
                 </a>
@@ -25,7 +25,7 @@ function Project({ image, name, live, source, desc }) {
                     href={source}
                     target='_blank'
                     rel='noreferrer'
-                    className='flex items-center gap-1 px-4 py-2 border border-black rounded-md text-sm bg-white dark:bg-[#20262E]'
+                    className='flex items-center gap-1 px-4 py-2 border border-black rounded-md text-sm bg-white dark:bg-gray-600 transition-scale hover:scale-105'
                 >
                     <AiFillGithub className='w-6 h-6' /> Code
                 </a>
